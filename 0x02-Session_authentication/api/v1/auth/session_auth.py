c3832c073a4e569416d63eba5e1f00f328724b41
safae <125465623+sh15935@users.noreply.github.com>
2024-04-19 00:06:53 +0100

    Add files via upload

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+""" Session authentication module"""
+from api.v1.auth.auth import Auth
+from models.user import User
+from uuid import uuid4
+
+
+class SessionAuth(Auth):
+    """ Session Authentication class"""
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ creates a Session ID for a user """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        session_id = str(uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """ return a User ID based on a Session ID """
+        if session_id is None or not isinstance(session_id, str):
+            return None
+        return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """ returns a User instance based on a cookie"""
+        session_cookie = self.session_cookie(request)
+        if session_cookie is None:
+            return None
+        _id = self.user_id_for_session_id(session_cookie)
+        return User.get(_id)
+
+    def destroy_session(self, request=None):
+        """ deletes the user session"""
+        if request is None:
+            return False
+        session_id = self.session_cookie(request)
+        if not session_id:
+            return False
+        user_id = self.user_id_for_session_id(session_id)
+        if not user_id:
+            return False
+        del self.user_id_by_session_id[session_id]
+        return True
